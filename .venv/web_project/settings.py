48f88b01a2fd779343ccd6b05d80fbb63ebe3c30
Athena2077 <rachel.hanlon@hotmail.com>
2022-09-25 22:31:47 +0100

    Sixth commit

--- .venv/web_project/settings.py
+++ .venv/web_project/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x488($6b48z^3meavjka#+_i4x(f%&efl6ci*@ak8dn-6l=!0d'
+#SECRET_KEY = 'django-insecure-x488($6b48z^3meavjka#+_i4x(f%&efl6ci*@ak8dn-6l=!0d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['developmentwiki.herokuapp.com']
 
 
 # Application definition
